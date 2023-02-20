2b06cb1da954bc5d4ccb4149f7df0985bda4b226
Niklas Koch <nk1000@gmx.de>
2023-02-20 12:03:30 +0100

    feat: dagger-hilt

---
+++ app/src/main/java/de/uniks/codecamp/group_a/fitnessapp/FitnessApp.kt
@@ -0,0 +1,8 @@
+package de.uniks.codecamp.group_a.fitnessapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FitnessApp: Application() {
+}
--- app/src/main/java/de/uniks/codecamp/group_a/fitnessapp/MainActivity.kt
+++ app/src/main/java/de/uniks/codecamp/group_a/fitnessapp/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import de.uniks.codecamp.group_a.fitnessapp.ui.theme.FitnessAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
